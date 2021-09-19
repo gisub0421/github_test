82916e71fd22647c38cb912d289b14a30abcaaea
Gisub <gisub0421@gmail.com>
2021-09-19 14:33:33 +0900

    telegram_message_first_test

---
+++ telebot_test.py
@@ -0,0 +1,12 @@
+import telepot
+import datetime
+
+
+
+token = "1508570773:AAHPyymSrNoJYhlfCmeVyEsazfa7YI_o1nE"
+mc = "1597576562"
+bot = telepot.Bot(token)
+
+message = "Time 231"
+
+bot.sendMessage(mc, message)
